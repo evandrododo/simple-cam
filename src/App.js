b107c8fb005760f7695a6a7988ee2659b93c2162
Evandro Carreira <evandro.carreira@gmail.com>
2019-01-23 11:45:21 -0200

    Salvando no Mongo em base64

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import Webcam from './Webcam';
+import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
+    const client = Stitch.initializeDefaultAppClient('uploadvideos-wckak');
+    const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('videos');
+    this.state = {
+      video: null,
+      videos: [],
+      webcamVisibility: false,
+      client,
+      db
+    }
     Notification.requestPermission(function(status) {
       console.log('Notification permission status:', status);
+    });
+  }
+
+  componentDidMount() {
+
+    const client = this.state.client
+    const db = this.state.db
+    
+    client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
+      db.collection('videos').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+    ).then(videos => {
+        this.setState({
+          video: null, 
+          videos
+        })
+    }).catch(err => {
+        console.error(err)
     });
   }
   
@@ -21,21 +49,109 @@ class App extends Component {
     }
   }
 
+  
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+
+  showWebcam = () => {
+    this.webcam.startCam();
+    this.setState({
+      webcamVisibility: true
+    })
+  }
+
+  hideWebcam = () => {
+    this.setState({
+      webcamVisibility: false
+    })
+  }
+
+  addVideo = (blobUrl, blob) => {
+    var myReader = new FileReader();
+    myReader.onloadend = (e) => {
+      console.log(e)
+      var blob64 = myReader.result;                
+      this.setState({
+      video: {
+        blobUrl,
+        blob64
+        }
+      })
+    }
+    myReader.readAsDataURL(blob); 
+  }
+
+  /*
+  sendVideo = () => {
+    const data = new FormData();
+    data.append('chunks', this.state.video.chunks);
+    fetch("http://localhost", {
+      mode: 'no-cors',
+      method: "POST",
+      body: data
+    }).then(function (res) {
+      if (res.ok) {
+        alert("Perfect! ");
+      } else if (res.status == 401) {
+        alert("Oops! ");
+      }
+    }, function (e) {
+      console.log("error submitting form", e)
+    });
+  }
+  */
+
+  saveVideo = () => {
+    const blob64 = this.state.video.blob64;
+    const client = this.state.client
+    const db = this.state.db
+    
+    client.auth.loginWithCredential(new AnonymousCredential()).then(user =>
+      db.collection('videos').insertOne({owner_id: client.auth.user.id, blob64 } )
+    ).then(() =>
+      db.collection('videos').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
+    ).then(docs => {
+        console.log("Found docs", docs)
+        console.log("[MongoDB Stitch] Connected to Stitch")
+        this.setState({
+          video: null
+        })
+    }).catch(err => {
+        console.error(err)
+    });
+  }
 
   render() {
+    let videos = this.state.videos.map( (video, index) => {
+      if( video.blob64) {
+        console.log(video.blob64)
+        return <video key={index} src={video.blob64} controls/>
+      }
+
+      if( !video.videoChunks) {
+        return null;
+      }
+      
+      //let blob = new Blob(atob(video.videoChunks), {type: "video/webm"});
+      let blobUrl = 'sdf'
+      //let blobUrl = URL.createObjectURL(video.videoBlob);
+      return <video key={index} src={blobUrl} controls/>
+    })
     return (
       <div className="App">
         <header className="App-header">
           <button onClick={this.displayNotification}>notifica-me</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.showWebcam}>adicionar video</button>
+          <Webcam ref={node => this.webcam = node} onRecord={this.addVideo} visibility={this.state.webcamVisibility} onClose={this.hideWebcam}/>
         </header>
+          {this.state.video && (
+            <div> 
+              <video src={this.state.video.blobUrl} controls/>
+              <button onClick={this.saveVideo}>Enviar video</button>
+            </div>
+          )}
+          {videos}
       </div>
     );
   }
---
+++ src/Webcam.js
@@ -0,0 +1,163 @@
+import React, {
+    Component
+} from 'react';
+import './webcam.scss';
+import { FaStop, FaTimes, FaExchangeAlt } from 'react-icons/fa';
+
+class Webcam extends Component {
+
+    setRef = preview => {
+        this.preview = preview;
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            record: false,
+            videoRecorder: null,
+            dataRecorded: null,
+            mediaDevices: [],
+            facingMode: 'environment',
+            log: ''
+        }
+    }
+
+    startRecording = () => {
+        let videoRecorder = new MediaRecorder(this.preview.captureStream());
+        let dataRecorded = [];
+
+        videoRecorder.ondataavailable = event => dataRecorded.push(event.data);
+        videoRecorder.start();
+        this.setState({
+            record: true,
+            videoRecorder,
+            dataRecorded,
+        });
+    }
+
+    stopRecording = () => {
+        this.preview.srcObject.getTracks().forEach(track => track.stop());
+        let videoRecorder = this.state.videoRecorder;
+        let stopped = new Promise((resolve, reject) => {
+            videoRecorder.onstop = resolve;
+            videoRecorder.onerror = event => reject(event.name);
+        });
+
+        videoRecorder.stop()
+        let recorded = () => {
+            videoRecorder.stop()
+            this.setState({
+                videoRecorder
+            });
+            return videoRecorder.state === "recording";
+        }
+        return Promise.all([
+                stopped,
+                recorded
+            ])
+            .then(() => this.state.dataRecorded);
+    }
+
+    saveRecord = () => {
+        this.stopRecording()
+        .then(recordedChunks => {
+            this.setState({
+                log: this.state.log+" - recordedchunks:"+recordedChunks.length
+            });
+            let recordedBlob = new Blob(recordedChunks, {
+                type: "video/webm"
+            });
+            let blobUrl = URL.createObjectURL(recordedBlob);
+
+            if(this.props.onRecord) {
+                this.props.onRecord(blobUrl, recordedBlob)
+            }
+            this.setState({
+                record: false,
+            });
+            this.closeWebcam();
+        })
+        .catch( error => {
+        });
+    }
+
+    startCam = () => {
+        navigator.mediaDevices.enumerateDevices()
+        .then( devices => {
+            let mediaDevices = [];
+            devices.forEach( device => {
+                if(device.kind === 'videoinput') {
+                    mediaDevices.push(device);
+                    console.log(device);
+                }
+            })
+            this.setState({
+                mediaDevices,
+                selectedDevice: mediaDevices[0]
+            })
+
+            var constraints = {
+                audio: true, 
+                video: {
+                    facingMode: this.state.facingMode
+                }
+            };
+            navigator.mediaDevices.getUserMedia(constraints)
+            .then(stream => {
+                this.preview.srcObject = stream;
+                this.preview.captureStream = this.preview.captureStream || this.preview.mozCaptureStream;
+            });
+        });
+    }
+
+    nextDevice = () => {
+        const facingMode = this.state.facingMode === 'environment' ? 'user' : 'environment';
+        this.setState({
+            facingMode
+        })
+
+        if(this.preview.srcObject) {
+            this.preview.srcObject.getTracks().forEach(track => track.stop());
+        }
+        const constraints = {
+            audio: true,
+            video: {
+                facingMode: this.state.facingMode
+            }
+        }
+            this.setState({
+                log: this.state.log+" - facingMode: "+JSON.stringify(constraints)
+            });
+        navigator.mediaDevices.getUserMedia(constraints)
+        .then(stream => {
+            this.preview.srcObject = stream;
+            this.preview.captureStream = this.preview.captureStream || this.preview.mozCaptureStream;
+        });
+    }
+
+    closeWebcam = () => {
+        if (this.props.onClose) this.props.onClose();
+    }
+
+    render() {
+        return <div className={this.props.visibility ? 'webcam-component':'webcam-component hidden'}>
+            <video ref={node => this.preview = node}  muted="muted" width="100%" height="100%" autoPlay={true}></video>
+            <button className="close-btn" onClick={this.closeWebcam}><FaTimes/></button>
+            <div className="cam-controls">
+                <div className="btn-record">
+                    { !this.state.record && (
+                    <button className="record" onClick={this.startRecording} type="button">&nbsp;</button>
+                    )}
+                    { this.state.record && (
+                    <button className="stop-record" onClick={this.saveRecord} type="button"><FaStop/></button>
+                    )}
+                </div>
+                { this.state.mediaDevices.length > 2 &&(
+                <button className="change-record" onClick={this.nextDevice} type="button"><FaExchangeAlt/></button>
+                )}
+            </div>
+        </div>
+    }
+}
+
+export default Webcam;
