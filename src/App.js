2bf832dfce59d7d3d01594ab0bc4699146ac78fa
Evandro Carreira <evandro.carreira@gmail.com>
2019-01-24 12:04:36 -0200

    CSS e status de notificação

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Webcam from './Webcam';
 import { Stitch, RemoteMongoClient, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+import { FaVideo } from 'react-icons/fa';
 
 class App extends Component {
 
@@ -10,18 +11,26 @@ class App extends Component {
     const client = Stitch.initializeDefaultAppClient('uploadvideos-wckak');
     const db = client.getServiceClient(RemoteMongoClient.factory, 'mongodb-atlas').db('videos');
     this.state = {
+      statusNotificacao: 'indefinido',
+      statusSW: '?',
       video: null,
       videos: [],
       webcamVisibility: false,
       client,
       db
     }
-    Notification.requestPermission(function(status) {
+  }
+  
+  componentDidMount() {
+    Notification.requestPermission((status) => {
+      this.setState({
+        statusNotificacao: status
+      })
       console.log('Notification permission status:', status);
     });
+    this.retrieveVideosFromDB();
   }
-
-  componentDidMount() {
+  retrieveVideosFromDB() {
     const client = this.state.client
     const db = this.state.db
     
@@ -40,15 +49,23 @@ class App extends Component {
   displayNotification = () => {
     if(!('serviceWorker' in navigator)) {
       console.log('sw not supported');
+      this.setState({
+        statusSW: 'not in navigator'
+      })
     }
+    console.log(navigator.serviceWorker);
     if (Notification.permission === 'granted') {
       navigator.serviceWorker.ready
       .then((serviceWorkerRegistration) => {
-          serviceWorkerRegistration.pushManager.getSubscription()
-          .then((subscription) => {
-                console.log('subscription', subscription.toJSON())
-          });
-          serviceWorkerRegistration.showNotification('Push notifications parecem simples')
+        this.setState({
+          statusSW: 'ready'
+        })
+        console.log('serviceWorkerRegistration', serviceWorkerRegistration)
+        serviceWorkerRegistration.pushManager.subscribe()
+        .then((subscription) => {
+          console.log('subscription', subscription)
+        });
+        serviceWorkerRegistration.showNotification('Push notifications parecem simples')
       });
     }
   }
@@ -144,17 +161,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <button onClick={this.displayNotification}>notifica-me</button>
-          <button onClick={this.showWebcam}>adicionar video</button>
+          <span>Permissão de notificação: <b>{this.state.statusNotificacao}</b></span>
+          <span>Service worker: <b>{this.state.statusSW}</b></span>
+          <button onClick={this.displayNotification}>Gerar Notificação</button>
           <Webcam ref={node => this.webcam = node} onRecord={this.addVideo} visibility={this.state.webcamVisibility} onClose={this.hideWebcam}/>
         </header>
+        <div className="flex-column">
+          <button className="show-cam" onClick={this.showWebcam}><FaVideo/></button>
           {this.state.video && (
-            <div> 
+            <div className="flex-column"> 
               <video src={this.state.video.blobUrl} controls/>
               <button onClick={this.saveVideo}>Enviar video</button>
             </div>
           )}
           {videos}
+        </div>
       </div>
     );
   }
